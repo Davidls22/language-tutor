764e0b2a6223e80bd33512b2999fde31e9fc9e6a
David Sugden <david-sugden1@hotmail.co.uk>
2025-02-19 08:31:14 +0000

    more styling changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,7 @@
   let { children } = $props();
   import "../app.css";
 </script>
-{@render children()}
+
+<div class="min-h-screen bg-teal-800 text-white flex justify-center items-start pt-6">
+  {@render children()}
+</div>
